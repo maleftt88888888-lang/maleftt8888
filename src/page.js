e343c8a0beffafd1f7980bd34d906aa1c153a102
maleftt88888888-lang <maleftt88888888@gmail.com>
2026-09-03 01:00:03 +0800

    Update page.js

--- src/page.js
+++ src/page.js
@@ -169,8 +169,8 @@ body {
 <body>
 <div class="topbar">
   <a class="back" href="/">← 主页</a>
-  <span class="topcredit">📺 <a class="ytname" href="小紅書獨家ID 1134717149" target="_blank" rel="noopener">YouTube CyberHandyman 小紅書獨家ID 1134717149</a><span class="forkline"> · fork from 鸣谢贡献者: Yu9191 / mekos2772 / acheong08 <span class="v11">· </span></span></span>
-  <a class="tg" href="https://t.me/cyberhandymancngroup" target="_blank" rel="noopener">✈️ TG群</a>
+  <span class="topcredit">📺 <a class="ytname" href="小紅書獨家ID 1134717149" target="_blank" rel="noopener">YouTube CyberHandyman 小紅書獨家ID 1134717149</a><span class="forkline"> · fork from 鸣谢贡献者:  <span class="v11">· </span></span></span>
+  <a class="tg" href="https://maleftt8888.maleftt88888888.workers.dev/" target="_blank" rel="noopener">✈️ </a>
 </div>
 <div class="redbox">
   <div class="rt">⚠️ 小紅書獨家ID 1134717149</div>
